30e569ccd5309d79678491bc2a484f780ab57558
zhulingyun <youthzhulingyun@163.com>
2017-12-12 23:42:04 +0800

    院线电影前端增加点击事件

---
+++ app/static/js/get_theatrical_film.js
@@ -0,0 +1,15 @@
+/**
+ * Created by zhulingyun on 2017/12/12.
+ */
+
+function get_theatrical_film(id) {
+    console.log($(id))
+    alert(id)
+    // $.get("/api/v1_0/theatrical_film", function (resp) {
+    //     if (resp.errno == 0) {
+    //
+    //     } else {
+    //         alert(resp.errmsg);
+    //     }
+    // })
+}
