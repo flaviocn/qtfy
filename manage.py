0e130f3f0336fd65f2bfa1411429eae14ec1fa5f
zhulingyun <youthzhulingyun@163.com>
2018-01-09 14:09:58 +0800

    页面修改

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # coding:utf-8
-from flask_admin import Admin, BaseView, expose
+
+from flask_admin import Admin
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
@@ -13,19 +14,19 @@ admin = Admin(app, name=u'后台管理系统')
 
 # 在这里初始化Flask Flask-SQLAlchemy Flask-Admin
 
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Movie, db.session))
-admin.add_view(ModelView(MovieComment, db.session))
-admin.add_view(ModelView(TheatricalFilm, db.session))
-admin.add_view(ModelView(TheatricalFilmComment, db.session))
-admin.add_view(ModelView(TvShow, db.session))
-admin.add_view(ModelView(TvShowNum, db.session))
-admin.add_view(ModelView(TvShowComment, db.session))
-admin.add_view(ModelView(Variety, db.session))
-admin.add_view(ModelView(VarietyNum, db.session))
-admin.add_view(ModelView(VarietyComment, db.session))
-admin.add_view(ModelView(News, db.session))
-admin.add_view(ModelView(Message, db.session))
+# admin.add_view(ModelView(User, db.session))
+# admin.add_view(ModelView(Movie, db.session))
+# admin.add_view(ModelView(MovieComment, db.session))
+# admin.add_view(ModelView(TheatricalFilm, db.session))
+# admin.add_view(ModelView(TheatricalFilmComment, db.session))
+# admin.add_view(ModelView(TvShow, db.session))
+# admin.add_view(ModelView(TvShowNum, db.session))
+# admin.add_view(ModelView(TvShowComment, db.session))
+# admin.add_view(ModelView(Variety, db.session))
+# admin.add_view(ModelView(VarietyNum, db.session))
+# admin.add_view(ModelView(VarietyComment, db.session))
+# admin.add_view(ModelView(News, db.session))
+# admin.add_view(ModelView(Message, db.session))
 
 babel = Babel(app)
 
